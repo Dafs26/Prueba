ca5459b65b430eeabc17ee7c33d4c3feeb0e416d
Dafs26 <62890508+Dafs26@users.noreply.github.com>
2020-04-06 22:16:45 -0400

    Add files via upload

---
+++ Primero/Primero/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Primero
+{
+    class Person
+    {
+        private string _name;
+        private int _age;
+        
+        private Person()
+        {
+            _name = string.Empty;
+            _age = 0;
+        }
+        public Person(string name, int age) : this()
+        {
+            _name = name;
+            _age = age;
+        }
+        public string Name { get => _name; set => _name = value; }
+        public int Age { get => _age; set => _age = value; }
+        
+        public void Salute()
+        {
+            Console.WriteLine("Hello "+_name+" with the age of "+_age);
+        }
+    }
+}
---
+++ Primero/Primero/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Primero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("David", 24);
+            person.Salute();
+        }
+    }
+}
